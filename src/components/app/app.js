5348b18e3c4a91b68681bc92580a3f664f00b04d
StanislavFedyna <stanislavfedyna@gmail.com>
2019-03-04 17:05:24 +0200

    +refac, included function for creating elements

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,11 +14,19 @@ export default class App extends React.Component{
 
   state = {
       todoData : [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awesome App', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 }
+        this.createTodoItem('Drink Coffee'),
+        this.createTodoItem('Make Awesome App'),
+        this.createTodoItem('Have a lunch')
       ]
     }
+  createTodoItem = (label) => {
+    return {
+      label,
+      important: false,
+      done: false,
+      id: this.maxId++
+    }
+  }
 
   deleteItem = (id) => {
     this.setState(({ todoData })=>{
@@ -35,12 +43,8 @@ export default class App extends React.Component{
   };
 
   addItem = (text) => {
-    const newItem = {
-      label: text,
-      important: false,
-      id: this.maxId++
-    }
-
+    const newItem = this.createTodoItem(text);
+    
     this.setState(({ todoData }) => {
       const newArr = [...todoData, newItem]; //додавання елементу без зміни помереднього
       return {
